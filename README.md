181cc27649fe15a19ad860d165d49e7a6e9ae29a
payamrastogi <payamrastogi@outlook.com>
2020-11-16 15:20:31 -0500

    add PushNotification class

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+
+
+```
+git update-index --assume-unchanged ./settings.yaml
+```
+
 References:
 - https://iotdesignpro.com/projects/home-security-system-using-raspberry-pi-and-pir-sensor-with-push-notification-alert
+- https://stackoverflow.com/questions/6964297/untrack-files-from-git-temporarily
--- pushNotification.py
+++ pushNotification.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
 from pushbullet import Pushbullet
-pb = Pushbullet("<access_token>")
-device = pb.get_device('<device_name>')
-device.push_note("Test", "Test Message")
+import yaml
+
+class PushNotification:
+    def __init__(self):
+        with open("settings.yaml", 'r') as stream:
+            try:
+                self.settings = yaml.safe_load(stream)
+                self.pushbullet = Pushbullet(self.settings['access_token'])
+                self.device = self.pushbullet.get_device(self.settings['device_name'])
+            except yaml.YAMLError as exc:
+                print(exc)
+    
+    def notify(self, notificationType, message):
+        self.device.push_note(notificationType, message)
+
+pushNotification = PushNotification()
+pushNotification.notify("Test", "Test Message")
